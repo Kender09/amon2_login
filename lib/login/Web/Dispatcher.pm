a001322ba110f6cc6691fc54f253089932e349ed
Kender09 <k.ikeuchi@dcl.cs.waseda.ac.jp>
2016-05-18 14:35:54 +0900

    papier mache

--- lib/login/Web/Dispatcher.pm
+++ lib/login/Web/Dispatcher.pm
@@ -4,6 +4,9 @@ use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
+use Data::Dumper;
+
+
 any '/' => sub {
     my ($c) = @_;
     my $counter = $c->session->get('counter') || 0;
@@ -11,6 +14,30 @@ any '/' => sub {
     $c->session->set('counter' => $counter);
     return $c->render('index.tx', {
         counter => $counter,
+    });
+};
+
+any '/login' => sub {
+    my ($c) = @_;
+    return $c->render('login.tx', {
+        message => $c->req->parameters->{message}
+    });
+};
+
+post '/login_pos' => sub {
+    my ($c) = @_;
+    warn Dumper $c->req->parameters->{id};
+    my $id = $c->req->parameters->{id};
+    $c->session->expire();
+    return $c->redirect('/login', {
+      message => " $id ログイン"
+    });
+};
+
+
+any '/sign_up' => sub {
+    my ($c) = @_;
+    return $c->render('sign_up.tx', {
     });
 };
 
